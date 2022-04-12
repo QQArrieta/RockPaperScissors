f8a1b4a90504c9388400af37ef6c76a2eeec9a89
Qneil Arrieta <arrietaqneil999@gmail.com>
2022-04-12 15:42:47 +0800

    Finish UI

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 
-/*Create function computerPlay()
- - Generate a random number from 1 - 3;
- - Use this random number to determine the computer's Action 
-   (1 - ROCK, 2 - PAPER, 3- SCISSOR)
- - Return the selection as a STRING 
- - Assign computerSelection = computerPlay();*/
 function computerPlay(){
  let selection = Math.floor((Math.random() * 3) + 1);
     switch(selection){
@@ -20,84 +14,113 @@ function computerPlay(){
     }
 }
 
-function playerPlay(){
-    let selection = prompt("Let's play a round of Rock, Paper, Scissors!", "(Please input either Rock, Paper,or Scissors)");
-    selection = selection.toLowerCase();
-    if (selection == "rock"|| selection == "paper"|| selection == "scissors")
-    { 
-        return selection;
-    }
-    else 
-        alert("Invalid Selection");
-        return 0;
+function win(){
+    playerWins += 1;
 }
-
-function playRound(playerSelection, computerSelection)
+function loss(){
+    computerWins+=1;
+}
+let playerWins = 0;
+let computerWins = 0;
+function playRound(e)
 {
+if (playerWins == 5){
+    alert("You have won the game! You have won 5 rounds!");
+    document.location.reload();
+}
+else if (computerWins == 5){
+    alert("You have lost the game! Computer has one 5 games.");
+    document.location.reload();
+}
+else{
+    computerSelection= computerPlay();
+    playerSelection= e.target.id;
+  
+    
     if (playerSelection == "rock" && computerSelection =="rock")
-    {
-        alert("It's a tie. You both chose rock");
-        return 0;
+    {   
+        const results = document.querySelector('#results');
+        const content = document.createElement('p');
+        content.textContent = "It's a tie! Both players chose rock";
+        results.appendChild(content);
     }
     else if (playerSelection == "rock" && computerSelection =="paper")
     {
-        alert("You lost. Paper beats rock.");
-        return 0;
+        loss();
+        const results = document.querySelector('#results');
+        const content = document.createElement('p');
+        content.textContent = "You lost! Computer chose paper";
+        results.appendChild(content);
+        
+        
+
     } 
     else if (playerSelection == "rock" && computerSelection =="scissors")
     {
-        alert("You won! Rock beats scissors.");
-        return 1;
+        win();
+        const results = document.querySelector('#results');
+        const content = document.createElement('p');
+        content.textContent = "You won! Computer chose scissors";
+        results.appendChild(content);
+        
     }
     else if (playerSelection == "paper" && computerSelection =="rock")
     {
-        alert("You won! Paper beats rock.");
-        return 1;
+        const results = document.querySelector('#results');
+        const content = document.createElement('p');
+        content.textContent = "You won! Computer chose rock";
+        results.appendChild(content);
+        win();
     }
     else if (playerSelection == "paper" && computerSelection == "paper")
     {
-        alert("Its a tie! You both chose paper");
-        return 0;
+        const results = document.querySelector('#results');
+        const content = document.createElement('p');
+        content.textContent = "It's a tie! Computer chose paper";
+        results.appendChild(content);
     }
     else if (playerSelection == "paper" && computerSelection == "scissors")
     {
-        alert("You lost! Scissors beats paper");
-        return 0;
+        const results = document.querySelector('#results');
+        const content = document.createElement('p');
+        content.textContent = "You lost! Computer chose scissors";
+        results.appendChild(content);
+        loss();
+    
     }
     else if (playerSelection == "scissors" && computerSelection == "rock")
     {
-        alert("You lost! Rock beats scissors");
-        return 0;
+        const results = document.querySelector('#results');
+        const content = document.createElement('p');
+        content.textContent = "You lost! Computer chose rock";
+        results.appendChild(content);
+        loss();
     }
     else if (playerSelection == "scissors" && computerSelection == "paper")
     {
-        alert("You won! Scissors beats paper");
-        return 1;
+        const results = document.querySelector('#results');
+        const content = document.createElement('p');
+        content.textContent = "You won! Computer chose paper";
+        results.appendChild(content);
+        win();
     }
     else if (playerSelection =="scissors" && computerSelection == "scissors")
     {
-        alert("It's a tie. You both chose scissors");
-        return 0;
+        const results = document.querySelector("#results");
+        const content = document.createElement('p');
+        content.textContent = "It's a tie! Computer chose scissors";
+        results.appendChild(content);
     }
-    else
-        alert("Invalid game");
-        return 0;
+    const playerScore = document.querySelector('#playerScore');
+    playerScore.textContent = `Player wins: ${playerWins}`;
+    scoreBoard.appendChild(playerScore);
+    const computerScore = document.querySelector('#computerScore');
+    computerScore.textContent = `Computer wins: ${computerWins}`;
+    scoreBoard.appendChild(computerScore);
+}
 }
 
-function game(){
-    alert("We're gonna play 5 rounds of Rock, Paper, Scissors");
-    let winCounter = 0;
-    for (let i = 0; i<5; i++)
-    {
-        let playerSelection;
-        let computerSelection;
-        playerSelection = playerPlay();
-        computerSelection = computerPlay();
-        winCounter += playRound(playerSelection, computerSelection);
+document.getElementById("rock").addEventListener("click", playRound);
+document.getElementById("paper").addEventListener("click", playRound);
+document.getElementById("scissors").addEventListener("click", playRound);
 
-    }
-    let output = 'You won ' + winCounter+ ' out of 5 rounds! Congrats!';
-    alert(output);
-}   
-
-game();
