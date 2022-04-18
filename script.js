4906e8a400446878774d834739bbf17182048c2d
Qneil Arrieta <arrietaqneil999@gmail.com>
2022-04-18 16:24:06 +0800

    Improve UI for RPS

--- script.js
+++ script.js
@@ -1,126 +1,94 @@
 
 function computerPlay(){
- let selection = Math.floor((Math.random() * 3) + 1);
-    switch(selection){
+ let chosen = Math.floor((Math.random() * 3) + 1);
+    switch(chosen){
         case 1:
+                document.getElementById("computerRock").classList.toggle("selected");
+                setTimeout(function (){document.getElementById("computerRock").classList.remove("selected")}, 700);
                 return "rock";
                 break;
         case 2: 
+            
+                document.getElementById("computerPaper").classList.toggle("selected");
+                setTimeout(function (){document.getElementById("computerPaper").classList.remove("selected")}, 700);
                 return "paper";
+                
                 break;
         case 3: 
+                document.getElementById("computerScissors").classList.toggle("selected");
+                setTimeout(function(){document.getElementById("computerScissors").classList.remove("selected")}, 700);
                 return "scissors";
+                
                 break;
     }
 }
 
-function win(){
-    playerWins += 1;
-}
-function loss(){
-    computerWins+=1;
-}
-let playerWins = 0;
-let computerWins = 0;
-function playRound(e)
+
+function playRound(selection)
 {
-if (playerWins == 5){
-    alert("You have won the game! You have won 5 rounds!");
-    document.location.reload();
-}
-else if (computerWins == 5){
-    alert("You have lost the game! Computer has one 5 games.");
-    document.location.reload();
-}
-else{
-    computerSelection= computerPlay();
-    playerSelection= e.target.id;
-  
+    computerSelection=computerPlay();
     
-    if (playerSelection == "rock" && computerSelection =="rock")
-    {   
-        const results = document.querySelector('#results');
-        const content = document.createElement('p');
-        content.textContent = "It's a tie! Both players chose rock";
-        results.appendChild(content);
-    }
-    else if (playerSelection == "rock" && computerSelection =="paper")
-    {
-        loss();
-        const results = document.querySelector('#results');
-        const content = document.createElement('p');
-        content.textContent = "You lost! Computer chose paper";
-        results.appendChild(content);
-        
-        
 
-    } 
-    else if (playerSelection == "rock" && computerSelection =="scissors")
+    if (selection == "rock" && computerSelection =="rock")
     {
-        win();
-        const results = document.querySelector('#results');
-        const content = document.createElement('p');
-        content.textContent = "You won! Computer chose scissors";
-        results.appendChild(content);
-        
+       document.querySelector(".combatLogText").textContent = "I'ts a tie, you both chose rock!";
     }
-    else if (playerSelection == "paper" && computerSelection =="rock")
+    else if (selection =="rock" && computerSelection == "paper")
     {
-        const results = document.querySelector('#results');
-        const content = document.createElement('p');
-        content.textContent = "You won! Computer chose rock";
-        results.appendChild(content);
-        win();
+        loss++;
+        document.querySelector(".combatLogText").textContent = "You lost. Computer chose paper!";
+        document.querySelector(".computerScore").textContent = `${loss}`;
+        return loss;
     }
-    else if (playerSelection == "paper" && computerSelection == "paper")
+    else if (selection == "rock" && computerSelection == "scissors")
     {
-        const results = document.querySelector('#results');
-        const content = document.createElement('p');
-        content.textContent = "It's a tie! Computer chose paper";
-        results.appendChild(content);
+        win++;
+        document.querySelector(".combatLogText").textContent = "You won! Computer chose scissors!";
+        document.querySelector(".playerScore").textContent = `${win}`;
+        return win;
     }
-    else if (playerSelection == "paper" && computerSelection == "scissors")
+    else if (selection == "paper" && computerSelection == "rock")
     {
-        const results = document.querySelector('#results');
-        const content = document.createElement('p');
-        content.textContent = "You lost! Computer chose scissors";
-        results.appendChild(content);
-        loss();
+        win++;
+        document.querySelector(".combatLogText").textContent = "You won! Computer chose rock!";
+        document.querySelector(".playerScore").textContent = `${win}`;
+        return win;
+    }
+    else if (selection == "paper" && computerSelection == "paper")
+    {
+        document.querySelector(".combatLogText").textContent = "I'ts a tie, you both chose paper!";
+    }
+    else if (selection == "paper" && computerSelection == "scissors")
+    {
+        loss++;
+        document.querySelector(".combatLogText").textContent = "You lost. Computer chose scissors!";
+        document.querySelector(".computerScore").textContent = `${loss}`;
+        return loss;
+    }
+    else if (selection == "scissors" && computerSelection == "rock")
+    {
+        loss++;
+        document.querySelector(".combatLogText").textContent = "You lost. Computer chose rock!";
+        document.querySelector(".computerScore").textContent = `${loss}`;
+        return loss;
+    }
+    else if (selection == "scissors" && computerSelection == "paper")
+    {
+        win++;
+        document.querySelector(".combatLogText").textContent = "You won! Computer chose paper!";
+        document.querySelector(".playerScore").textContent = `${win}`;
+        return win;
+    }
+    else if (selection == "scissors" && computerSelection == "scissors")
+    {
+        document.querySelector(".combatLogText").textContent = "It's a tie, you both chose scissors!";
+    }
     
-    }
-    else if (playerSelection == "scissors" && computerSelection == "rock")
-    {
-        const results = document.querySelector('#results');
-        const content = document.createElement('p');
-        content.textContent = "You lost! Computer chose rock";
-        results.appendChild(content);
-        loss();
-    }
-    else if (playerSelection == "scissors" && computerSelection == "paper")
-    {
-        const results = document.querySelector('#results');
-        const content = document.createElement('p');
-        content.textContent = "You won! Computer chose paper";
-        results.appendChild(content);
-        win();
-    }
-    else if (playerSelection =="scissors" && computerSelection == "scissors")
-    {
-        const results = document.querySelector("#results");
-        const content = document.createElement('p');
-        content.textContent = "It's a tie! Computer chose scissors";
-        results.appendChild(content);
-    }
-    const playerScore = document.querySelector('#playerScore');
-    playerScore.textContent = `Player wins: ${playerWins}`;
-    scoreBoard.appendChild(playerScore);
-    const computerScore = document.querySelector('#computerScore');
-    computerScore.textContent = `Computer wins: ${computerWins}`;
-    scoreBoard.appendChild(computerScore);
 }
-}
+let win = 0;
+let loss = 0;
 
-document.getElementById("rock").addEventListener("click", playRound);
-document.getElementById("paper").addEventListener("click", playRound);
-document.getElementById("scissors").addEventListener("click", playRound);
+document.images[0].onclick = () => playRound("rock");
+document.images[1].onclick = () => playRound("paper");
+document.images[2].onclick = () => playRound("scissors");
 
